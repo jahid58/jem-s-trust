2623eca588ce93e03f68273c07637662fb76cdda
Jahid Hasan <jahidjjaa@gmail.com>
2022-04-04 01:59:41 +0600

    update

--- pages/api/doctors.tsx
+++ pages/api/doctors.tsx
@@ -1,7 +1,28 @@
-import * as clientPromise from '../../lib/mongodb'
+import { MongoClient } from 'mongodb'
+
+const options: Object = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+let client
+let clientPromise: any
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Please add your Mongo URI to .env.local')
+// }
+
+client = new MongoClient(
+  'mongodb+srv://Jahid:jsashsisds@cluster0.fdjas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  options
+)
+clientPromise = client.connect()
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
 
 export default async function handler(req: any, res: any) {
-  const client: any = await clientPromise
+  const client = await clientPromise
   const db = client.db("jem'sTrustDb")
   switch (req.method) {
     case 'POST':
